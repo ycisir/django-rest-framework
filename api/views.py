cbc2455c4dcbc00ae0cf3d2f4db115b60cfd5706
yasir <mohammadyasirkmt@gmail.com>
2024-07-04 13:11:47 +0530

    first commit

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from api.models import Student
+from api.serializers import StudentSerializer
+from rest_framework.renderers import JSONRenderer
+from django.http import HttpResponse
 
-# Create your views here.
+# Model object - single object
+def student_detail(request):
+    student = Student.objects.get(pk=1)
+    serializer = StudentSerializer(student)
+    json_data = JSONRenderer().render(serializer.data)
+    return HttpResponse(json_data, content_type='application/json')
