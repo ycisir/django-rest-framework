a92ab4015a060b00e0c6dee5d3472bb90d8b50d9
yasir <mohammadyasirkmt@gmail.com>
2024-07-12 06:49:48 +0530

    generic api view crud commit

---
+++ generic_api_view_crud/api/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from api.models import Student
+from api.serializers import StudentSerializer
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
+
+# List and Create - Where PK is not required - group 1
+class StudentCreateAndListAPI(GenericAPIView, ListModelMixin, CreateModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+    
+
+# Specific task where PK is required - group 2
+class StudentRetrieveUpdateAndDestroyAPI(GenericAPIView, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+   
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
