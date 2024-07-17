c64f917e55a583b0ffb4a12081e6263e97adae5b
yasir <mohammadyasirkmt@gmail.com>
2024-07-17 16:27:52 +0530

    token auth httpie commit

---
+++ token_auth_httpie/api/views.py
@@ -0,0 +1,14 @@
+from api.models import Student
+from api.serializers import StudentSerializer
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+class StudentModelViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
